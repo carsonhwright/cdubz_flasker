0a62fbc0e337d85ff2c26f9f468020d95edc57f1
carsonhwright <carsonhwright@gmail.com>
2026-02-27 01:05:13 +0000

    better file behavior, I'll need to figure out how to treat different links as downloadable or just "openable"

--- cdflasker/app.py
+++ cdflasker/app.py
@@ -15,8 +15,15 @@ def download_file(filename):
     return send_from_directory(
         FILES_DIR,
         filename,
-        as_attachment=True
+        as_attachment=False #True if you want it to download
     )
+
+
+# @app.route("/files/")
+# def list_files():
+#     files = os.listdir(FILES_DIR)
+#     file_links = [f'<li><a href="/files/{f}" target="_blank">{f}</a></li>' for f in files]
+#     return f"<h1>Files</h1><ul>{''.join(file_links)}</ul>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
