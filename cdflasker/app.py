500f0df94029bff46502f4c148ce7afbaad1282d
carsonhwright <carsonhwright@gmail.com>
2026-03-01 05:08:29 +0000

    experimenting with some output

--- cdflasker/app.py
+++ cdflasker/app.py
@@ -1,17 +1,27 @@
+import os
+
 from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
 
-FILES_DIR = "/root/cdubz_flasker/cdflasker/files"
+app = Flask(__name__, template_folder="templates")
+
+# FILES_DIR = "/root/cdubz_flasker/cdflasker/files"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FILES_DIR = os.path.join(BASE_DIR, "files")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    try:
+        files = os.listdir(FILES_DIR)
+    except FileNotFoundError:
+        files = []
+    files.sort()
+    return render_template("index.html", files=files)
 
 # /root/cdubz_flasker/cdflasker/templates/shadow_wizard_money.jpg
 # /root/cdubz_flasker/templates
 @app.route("/<path:filename>")
-def download_file(filename):
+def serve_file(filename):
     return send_from_directory(
         FILES_DIR,
         filename,
